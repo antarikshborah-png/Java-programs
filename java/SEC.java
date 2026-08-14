ffaa6a46296002188adc1ded985250b7ac036113
antarikshborah-png <antarikshborah@gmail.com>
2026-08-14 19:48:36 +0530

    Add files via upload

---
+++ java/SEC.java
@@ -0,0 +1,23 @@
+
+//     public static void main(String[] args) {
+//       Scanner sc=new Scanner(System.in);
+//       System.out.println("Enter a number");
+//       int a = sc.nextInt();
+//       if(a%2==0){
+//       System.out.println("The number is even");
+//     }
+//     else {
+//         System.out.println("The number is odd");
+//     }
+    
+
+//     }
+// }
+
+class SEC {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
